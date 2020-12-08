fc3e6b7c22772fdf7e1a44e8dda4eed45909bb3b
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2020-12-08 20:20:44 +0100

    Update day6.py

--- Others/Advent of Code/2020/day6.py
+++ Others/Advent of Code/2020/day6.py
@@ -2179,4 +2179,3 @@ zwynrv
 uvxym""".split('\n\n')
 part1=sum([len(set(i.replace('\n',''))) for i in strin])
 part2=sum([len(reduce(lambda x,y:x.intersection(y),[set(j) for j in i.split('\n')])) for i in strin])
-print(part1,part2)
