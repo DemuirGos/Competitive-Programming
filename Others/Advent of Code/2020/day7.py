c8be0ba48d96a821a9b0b60faae24e62d71acd7c
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2020-12-08 20:48:55 +0100

    Update day7.py

--- Others/Advent of Code/2020/day7.py
+++ Others/Advent of Code/2020/day7.py
@@ -599,7 +599,7 @@ p={}
 r={}
 for s in strin.split('\n'):
     matches = re.findall("(\d+)? ?(\w+ \w+) bag",s)
-    m[matches[0][1]] = matches[1:]
+    m[matches[0][1]] = dict([i[::-1] for i in matches[1:]])
 
 def canHoldGold (s,k) :
     if k in s:
@@ -620,9 +620,11 @@ def bagCanHold (k) :
         return r[k]
     size=0
     for bagTuple in m[k]:
-        if(bagTuple == ('', 'no other')):
+        if(bagTuple == 'no other'):
             continue
-        size+=int(bagTuple[0]) + int(bagTuple[0])*bagCanHold(bagTuple[1])
-    return size
-part1=len([b for b in m if canHoldGold(b,"shiny gold")])
-part2=bagCanHold("shiny gold")   
+        size+=int(m[k][bagTuple]) + int(m[k][bagTuple])*bagCanHold(bagTuple)
+    r[k]=size
+    return r[k]
+part1=len([b for b in m if canHoldGold(m[b],"shiny gold")])
+part2=bagCanHold("shiny gold")  
+print(part1,part2)
