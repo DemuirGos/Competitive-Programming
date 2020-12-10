8cd777920ad6281f5d7dfc51c2f7b459206cf517
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2020-12-10 22:51:03 +0100

    Update day7.py

--- Solutions/Others/Advent of Code/2020/day7.py
+++ Solutions/Others/Advent of Code/2020/day7.py
@@ -6,9 +6,7 @@ d={'flatten':lambda l,r:d['flatten'](sum([i for i in l if type(i)!=str],[]),r+[i
 def WayUpWeGo (s,k) :
     return [[b,WayUpWeGo(m,b)] for b in s if k in m[b]]
 def WayDownWeGo (k,s,size) :
-    if len(s)==0 :
-        return size
-    else :
-        return WayDownWeGo(k,s[1:],size + (s[0] != 'no other' and int(m[k][s[0]]) * ( 1 + WayDownWeGo(s[0],list(m[s[0]].keys()),0) or 0)))
+    return size if len(s)==0 else WayDownWeGo(k,s[1:],size + (s[0] != 'no other' and int(m[k][s[0]]) * ( 1 + WayDownWeGo(s[0],list(m[s[0]].keys()),0) or 0)))
 part1=len(d['flatten'](sum([[b,list(itertools.chain(*WayUpWeGo(m,b)))] for b in m if "shiny gold" in m[b]],[]),[]))
 part2=WayDownWeGo("shiny gold","shiny gold" in m and list(m["shiny gold"].keys()) or [],0) 
+print(part1,part2)
