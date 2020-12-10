222c06a80ea5b7bcc98ba4f46fcf37ad9c17b7cc
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2020-12-10 10:38:47 +0100

    Update day7.py

--- Solutions/Others/Advent of Code/2020/day7.py
+++ Solutions/Others/Advent of Code/2020/day7.py
@@ -1,14 +1,8 @@
 import sys
 import re
 strin = open("InputData\day7.txt", "r").read()
-m={}
-p={}
-r={}
-for s in strin.split('\n'):
-    matches = re.findall("(\d+)? ?(\w+ \w+) bag",s)
-    m[matches[0][1]] = dict([i[::-1] for i in matches[1:]])
-
-def canHoldGold (s,k) :
+m=dict((w[0][1],dict([i[::-1] for i in w[1:]])) for w in [re.findall("(\d+)? ?(\w+ \w+) bag",s) for s in strin.split('\n')])
+def canHoldGold (s,k,p) :
     if k in s:
         return True
     val = False
@@ -17,21 +11,20 @@ def canHoldGold (s,k) :
             val |= p[bag]
         else:
             if bag in m:
-                p[bag]=canHoldGold(m[bag],k)
+                p[bag]=canHoldGold(m[bag],k,p)
             else:
                 p[bag]=False
             val |= p[bag]
     return val
-def bagCanHold (k) :
+def bagCanHold (k,r) :
     if k in r :
         return r[k]
     size=0
     for bagTuple in m[k]:
         if(bagTuple == 'no other'):
             continue
-        size+=int(m[k][bagTuple]) + int(m[k][bagTuple])*bagCanHold(bagTuple)
+        size+=int(m[k][bagTuple]) + int(m[k][bagTuple])*bagCanHold(bagTuple,r)
     r[k]=size
     return r[k]
-part1=len([b for b in m if canHoldGold(m[b],"shiny gold")])
-part2=bagCanHold("shiny gold")  
-print(part1,part2)
+part1=len([b for b in m if canHoldGold(m[b],"shiny gold",{})])
+part2=bagCanHold("shiny gold",{})  
