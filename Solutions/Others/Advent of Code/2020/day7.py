3b901b34890b6a523f7e65fade76d5dbdf742754
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2020-12-10 13:15:50 +0100

    Update day7.py

--- Solutions/Others/Advent of Code/2020/day7.py
+++ Solutions/Others/Advent of Code/2020/day7.py
@@ -2,29 +2,20 @@ import sys
 import re
 strin = open("InputData\day7.txt", "r").read()
 m=dict((w[0][1],dict([i[::-1] for i in w[1:]])) for w in [re.findall("(\d+)? ?(\w+ \w+) bag",s) for s in strin.split('\n')])
-def canHoldGold (s,k,p) :
+def canHoldGold (s,k) :
     if k in s:
         return True
-    val = False
-    for bag in s:
-        if bag in p:
-            val |= p[bag]
+    def lookup(bags,cond):
+        if len(bags)==0:
+            return cond
         else:
-            if bag in m:
-                p[bag]=canHoldGold(m[bag],k,p)
-            else:
-                p[bag]=False
-            val |= p[bag]
-    return val
-def bagCanHold (k,r) :
-    if k in r :
-        return r[k]
-    size=0
-    for bagTuple in m[k]:
-        if(bagTuple == 'no other'):
-            continue
-        size+=int(m[k][bagTuple]) + int(m[k][bagTuple])*bagCanHold(bagTuple,r)
-    r[k]=size
-    return r[k]
-part1=len([b for b in m if canHoldGold(m[b],"shiny gold",{})])
-part2=bagCanHold("shiny gold",{})  
+            return lookup(bags[1:],bags[0] in m and cond|canHoldGold(m[bags[0]],k) or cond)
+    return lookup(list(s.keys()),False)
+def bagCanHold (k,s,size) :
+    if len(s)==0 :
+        return size
+    else :
+        return bagCanHold(k,s[1:],size + (s[0] != 'no other' and int(m[k][s[0]]) * ( 1 + bagCanHold(s[0],list(m[s[0]].keys()),0) or 0)))
+part1=len([b for b in m if canHoldGold(m[b],"shiny gold")])
+part2=bagCanHold("shiny gold","shiny gold" in m and list(m["shiny gold"].keys()) or [],0)  
+
