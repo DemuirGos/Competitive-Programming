920189a0bda5e8e66e68c8a5cce4208e96618967
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2020-12-11 16:38:53 +0100

    Update day9.py

--- Solutions/Others/Advent of Code/2020/day9.py
+++ Solutions/Others/Advent of Code/2020/day9.py
@@ -1,13 +1,4 @@
 strin=list(map(int,open("InputData\day9.txt", "r").read().split('\n')))
 keysetsize = 25
 part1 = sorted([(i,strin[i+keysetsize]) for i in range(len(strin[keysetsize:])) if len([(j,k) for j in strin[i:i+keysetsize] for k in strin[i:i+keysetsize] if k + j == strin[i+keysetsize]])==0])
-memo=set()
-def fracture(v,l,r,n):
-    if (l,r) not in memo:
-        memo.add((l,r))
-        if sum(v[l:r])> n:
-            fracture(v,l,r-1,n)
-            fracture(v,l+1,r,n)
-        elif sum(v[l:r])==n:
-            print('part2:',sum([min(v[l:r]),max(v[l:r])])) 
-fracture(strin,0,part1[0][0]-1,part1[0][1])
+part2 = (lambda x: min(x)+max(x))(*[strin[i:j] for i in range(len(strin[:part1[0][0]])) for j in range(len(strin[:part1[0][0]])) if sum(strin[i:j])==part1[0][1] and i!=part1[0][0]])
