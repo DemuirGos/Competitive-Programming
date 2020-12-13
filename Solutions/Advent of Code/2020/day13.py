96a0ce99186b3de56d3a696afec6621e048d75c6
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2020-12-13 19:13:33 +0100

    Update day13.py

--- Solutions/Advent of Code/2020/day13.py
+++ Solutions/Advent of Code/2020/day13.py
@@ -7,7 +7,7 @@ def RemainderCalculator(prt2):
             return t if t>=0 else t + init
         else:
             quotient = r // newr
-            return inverse(init, t - quotient * newt, r - quotient * newr, newt,newr)
+            return Inverse(init, t - quotient * newt, r - quotient * newr, newt,newr)
     Ids = list(map(lambda x:x[0],prt2))
     idx = list(map(lambda x:(x[0]-x[1])%x[0],prt2))
     n   = reduce(lambda x,y:x*y,Ids)
@@ -15,5 +15,5 @@ def RemainderCalculator(prt2):
     zi  = list(map(lambda x:Inverse(x[1],gcd(x[0],x[1]),x[0],0,x[1])%x[0],list(zip(yi,Ids))))
     return  reduce(lambda x,y:x+y,list(map(lambda x:x[0]*x[1]*x[2],list(zip(idx,yi,zi)))))%n
 part1 = (lambda x:(x[1][0][1]-x[0][0])*x[1][0][0])([strin[0][0],sorted([(x,ceil(strin[0][0][0]/x)*x,(ceil(strin[0][0][0]/x)*x)%strin[0][0][0],strin[1][0]) for x,i in strin[1]],key=lambda x:x[2])])
-part2 = part2sol(strin[1])
-print(part2)
+part2 = RemainderCalculator(strin[1])
+print(part1,part2)
