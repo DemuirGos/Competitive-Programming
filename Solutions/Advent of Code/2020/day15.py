84d1b4f382aa1621441d3c2c3912aba49bda1081
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2020-12-16 11:40:30 +0100

    Update day15.py

--- Solutions/Advent of Code/2020/day15.py
+++ Solutions/Advent of Code/2020/day15.py
@@ -1,6 +1,6 @@
 strin=list(enumerate(map(int,open("InputData\day15.txt", "r").read().split(','))))
 def process(l,starters):
-    r,dictionary=starters[:-1][1],{n:(i) for i,n in starters}
+    r,dictionary=starters[-1][1],{n:(i) for i,n in starters}
     for i in range(len(starters)-1,l-1):
         dictionary[r],r =i, i - dictionary.get(r,i)
     return r
